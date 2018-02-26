183a63847d7cc744da3ebb1542696ba7e06b70b5
Michael Pope <popemray1986@gmail.com>
2018-02-26 23:20:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 11, "m", "brown", "happy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bob", 11, "m", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 11, "m", "brown", "happy", "true");
